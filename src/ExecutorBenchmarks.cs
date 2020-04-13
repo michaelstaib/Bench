55c2d0344d892841b1677d535429c45cd7d9d997
Michael Staib <michael@chillicream.com>
2020-04-13 13:22:49 +0200

    added more tests

--- src/ExecutorBenchmarks.cs
+++ src/ExecutorBenchmarks.cs
@@ -1,13 +1,12 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 using Bench.Data;
 using Benchmark.src.GraphQLDotNet;
 using BenchmarkDotNet.Attributes;
 using GraphQL;
 using GraphQL.Server.Internal;
 using HotChocolate.Execution;
-using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace Bench
 {
@@ -48,6 +47,127 @@ namespace Bench
         }
 
         [Benchmark]
+        public async Task<IExecutionResult> HotChocolate_SmallQuery_With_Fragments()
+        {
+            var request = QueryRequestBuilder.New()
+                .SetQuery(@"
+                {
+                    hero(episode: EMPIRE) {
+                        id
+                        name
+                        appearsIn
+                        friends {
+                            id
+                            name
+                            appearsIn
+                            height
+                            ... on Droid {
+                                primaryFunction
+                            }
+                            ... on Human {
+                                homePlanet
+                            }
+                        }
+                    }
+                }
+                ")
+                .SetServices(_services)
+                .Create();
+
+            return await _queryExecutor.ExecuteAsync(request);
+        }
+
+        [Benchmark]
+        public async Task<IExecutionResult> HotChocolate_SmallQuery_Introspection()
+        {
+            var request = QueryRequestBuilder.New()
+                .SetQuery(@"
+                  query IntrospectionQuery {
+    __schema {
+      queryType { name }
+      mutationType { name }
+      subscriptionType { name }
+      types {
+        ...FullType
+      }
+      directives {
+        name
+        description
+        args {
+          ...InputValue
+        }
+        onOperation
+        onFragment
+        onField
+      }
+    }
+  }
+
+  fragment FullType on __Type {
+    kind
+    name
+    description
+    fields(includeDeprecated: true) {
+      name
+      description
+      args {
+        ...InputValue
+      }
+      type {
+        ...TypeRef
+      }
+      isDeprecated
+      deprecationReason
+    }
+    inputFields {
+      ...InputValue
+    }
+    interfaces {
+      ...TypeRef
+    }
+    enumValues(includeDeprecated: true) {
+      name
+      description
+      isDeprecated
+      deprecationReason
+    }
+    possibleTypes {
+      ...TypeRef
+    }
+  }
+
+  fragment InputValue on __InputValue {
+    name
+    description
+    type { ...TypeRef }
+    defaultValue
+  }
+
+  fragment TypeRef on __Type {
+    kind
+    name
+    ofType {
+      kind
+      name
+      ofType {
+        kind
+        name
+        ofType {
+          kind
+          name
+        }
+      }
+    }
+  }
+
+                ")
+                .SetServices(_services)
+                .Create();
+
+            return await _queryExecutor.ExecuteAsync(request);
+        }
+
+        [Benchmark]
         public async Task<ExecutionResult> GQLDotNet_SmallQuery()
         {
             return await _gqlDotNetExecutor.ExecuteAsync("",
@@ -58,6 +178,118 @@ namespace Bench
                         name
                     }
                 }
+                ", null, null);
+        }
+
+        [Benchmark]
+        public async Task<ExecutionResult> GQLDotNet_SmallQuery_With_Fragments()
+        {
+            return await _gqlDotNetExecutor.ExecuteAsync("",
+                @"
+                {
+                    hero(episode: EMPIRE) {
+                        id
+                        name
+                        appearsIn
+                        friends {
+                            id
+                            name
+                            appearsIn
+                            height
+                            ... on Droid {
+                                primaryFunction
+                            }
+                            ... on Human {
+                                homePlanet
+                            }
+                        }
+                    }
+                }
+                ", null, null);
+        }
+
+        [Benchmark]
+        public async Task<ExecutionResult> GQLDotNet_SmallQuery_Introspection()
+        {
+            return await _gqlDotNetExecutor.ExecuteAsync("",
+                @"
+                query IntrospectionQuery {
+    __schema {
+      queryType { name }
+      mutationType { name }
+      subscriptionType { name }
+      types {
+        ...FullType
+      }
+      directives {
+        name
+        description
+        args {
+          ...InputValue
+        }
+        onOperation
+        onFragment
+        onField
+      }
+    }
+  }
+
+  fragment FullType on __Type {
+    kind
+    name
+    description
+    fields(includeDeprecated: true) {
+      name
+      description
+      args {
+        ...InputValue
+      }
+      type {
+        ...TypeRef
+      }
+      isDeprecated
+      deprecationReason
+    }
+    inputFields {
+      ...InputValue
+    }
+    interfaces {
+      ...TypeRef
+    }
+    enumValues(includeDeprecated: true) {
+      name
+      description
+      isDeprecated
+      deprecationReason
+    }
+    possibleTypes {
+      ...TypeRef
+    }
+  }
+
+  fragment InputValue on __InputValue {
+    name
+    description
+    type { ...TypeRef }
+    defaultValue
+  }
+
+  fragment TypeRef on __Type {
+    kind
+    name
+    ofType {
+      kind
+      name
+      ofType {
+        kind
+        name
+        ofType {
+          kind
+          name
+        }
+      }
+    }
+  }
                 ", null, null);
         }
     }
